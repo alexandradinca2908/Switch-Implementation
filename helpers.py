b9dc8bdde618e52047e53928833e3813339f2611
student <student@pcom.localdomain>
2024-10-31 20:25:31 +0200

    Implemented STP support

--- helpers.py
+++ helpers.py
@@ -1,4 +1,68 @@
 import wrapper
+import struct
+from wrapper import send_to_link
+
+BROADCAST = "ff:ff:ff:ff:ff:ff"
+MULTICAST = "01:80:c2:00:00:00"
+MULTICAST_BYTES = bytes.fromhex("0180C2000000")
+
+class BPDU:
+    def __init__(self, src_mac, root_bridge_ID, root_path_cost, 
+                 sender_bridge_ID, interface, message_age):
+        self.dst_mac = MULTICAST_BYTES
+        self.src_mac = src_mac
+        self.llc_length = 0x0026
+        self.dsap = 0x42
+        self.ssap = 0x42
+        self.control = 0x03
+        self.padding = 0x00000000
+        self.flags = 0x00
+        self.root_bridge_ID = root_bridge_ID
+        self.root_path_cost = root_path_cost
+        self.sender_bridge_ID = sender_bridge_ID
+        self.port_ID = interface
+        self.message_age = message_age
+        self.max_age = 0x0014
+        self.hello_time = 0x0002
+        self.forward_delay = 0x000F
+
+    def len(self): return 52
+
+    def pack(self):
+        pack = struct.pack('!6s6sHBBBIBQIQHHHHH', 
+                           self.dst_mac, self.src_mac, self.llc_length, self.dsap, 
+                           self.ssap, self.control, self.padding, self.flags, 
+                           self.root_bridge_ID, self.root_path_cost, self.sender_bridge_ID, 
+                           self.port_ID, self.message_age, self.max_age, self.hello_time, 
+                           self.forward_delay)
+        return pack
+
+def stp_init(vlan_table, own_bridge_ID):
+    # Init all switches as root
+    root_bridge_ID = own_bridge_ID
+    root_path_cost = 0
+    root_port = -1
+ 
+    trunk_interfaces = dict()
+
+    for i in vlan_table:
+        if vlan_table[i] == 'T':
+            trunk_interfaces[i] = "Designated"
+
+    return trunk_interfaces, [root_bridge_ID, root_path_cost, root_port]
+
+def unpack_buff(data):
+    src_mac = data[6:12]
+    root_bridge_ID = int.from_bytes(data[22:30], 'big')
+    root_path_cost = int.from_bytes(data[30:34], 'big')
+    bridge_ID =int.from_bytes(data[34:42], 'big')
+    interface = int.from_bytes(data[42:44], 'big')
+    message_age = int.from_bytes(data[44:46], 'big')
+
+    bpdu = BPDU(src_mac, root_bridge_ID, root_path_cost,
+                bridge_ID, interface, message_age)
+    
+    return bpdu
 
 def config_switch(switch_id, interfaces):
     vlan_table = dict()
@@ -6,7 +70,7 @@ def config_switch(switch_id, interfaces):
     f = open("configs/switch{}.cfg".format(switch_id), "r")
 
     # Read switch priority
-    switch_prio = int(f.readline().strip())
+    switch_prio_value = int(f.readline().strip())
 
     # Read VLAN configs
     lines = f.readlines()
@@ -16,4 +80,209 @@ def config_switch(switch_id, interfaces):
             if config[0] == wrapper.get_interface_name(i):
                 vlan_table[i] = config[1]
 
-    return vlan_table, switch_prio
+    return vlan_table, switch_prio_value
+
+def create_vlan_tag(vlan_id):
+    # 0x8100 for the Ethertype for 802.1Q
+    # vlan_id & 0x0FFF ensures that only the last 12 bits are used
+    return struct.pack('!H', 0x8200) + struct.pack('!H', vlan_id & 0x0FFF)
+
+def handle_bdpu(data, root, interface, trunk_interfaces, src_mac_bytes,
+                own_bridge_ID):
+    # Unpack bpdu
+    bpdu = unpack_buff(data)
+
+    # Unpack current root values
+    root_bridge_ID = root[0]
+    root_path_cost = root[1]
+    root_port_ID = root[2]
+
+    # Updating root
+    if (bpdu.root_bridge_ID < root_bridge_ID and 
+        bpdu.message_age < bpdu.max_age):
+        root_bridge_ID = bpdu.root_bridge_ID
+
+        # Add 10 to cost (all links are 100 Mbps)
+        root_path_cost = bpdu.root_path_cost + 10 
+
+        # Update root port
+        root_port_ID = interface
+
+        for i in trunk_interfaces:
+                if i != root_port_ID:
+                    trunk_interfaces[i] = "Blocking"
+
+        trunk_interfaces[root_port_ID] == "Designated"
+
+        # Update and forward this BPDU to all other trunk ports
+        for i in trunk_interfaces:
+            if i != interface:
+                fwd_bpdu = BPDU(src_mac_bytes, root_bridge_ID, root_path_cost,
+                                own_bridge_ID, i, bpdu.message_age + 1)
+                send_to_link(i, fwd_bpdu.len(), fwd_bpdu.pack())
+    
+    # Unchanged root
+    elif (bpdu.root_bridge_ID == root_bridge_ID and 
+            bpdu.message_age < bpdu.max_age):
+
+        # Same port, better cost
+        if (interface == root_port_ID and 
+            bpdu.root_path_cost + 10 < root_path_cost):
+            root_path_cost = bpdu.sender_path_cost + 10
+
+        # Different port, better cost
+        elif (interface != root_port_ID and 
+                bpdu.root_path_cost > root_path_cost):
+            trunk_interfaces[interface] = "Designated"
+
+    # Block ports that send back switch's BPDU
+    elif (bpdu.sender_bridge_ID == own_bridge_ID and 
+            bpdu.message_age < bpdu.max_age):
+        trunk_interfaces[interface] = "Blocking"
+
+    if own_bridge_ID == root_bridge_ID:
+        for i in trunk_interfaces:
+            trunk_interfaces[i] = "Designated"
+
+    # Update own info
+    root[0] = root_bridge_ID
+    root[1] = root_path_cost
+    root[2] = root_port_ID
+
+def send_frame(dest_mac, mac_table, vlan_table, vlan_id, 
+               interface, interfaces, data, length, trunk_interfaces):
+    # Forward from access or trunk port, depending on vlan_id
+    # Access
+    if vlan_id == -1:
+        fwd_from_access(dest_mac, mac_table, vlan_table, interface,
+                        interfaces, data, length, trunk_interfaces)
+
+    # Trunk
+    else:
+        fwd_from_trunk(dest_mac, mac_table, vlan_table, vlan_id,
+                       interface, interfaces, data, length, trunk_interfaces)
+
+# Switch receives a frame from access and forwards it
+def fwd_from_access(dest_mac, mac_table, vlan_table, interface,
+                    interfaces, data, length, trunk_interfaces):
+    # Unicast
+    if dest_mac != BROADCAST:
+
+        # Destination MAC is known
+        if dest_mac in mac_table:
+
+            # Calculate VLANs, frame with tag and destination interface
+            vlan_src = vlan_table[interface]
+            dest_interface = mac_table[dest_mac]
+            vlan_dest = vlan_table[dest_interface]
+            tagged_frame = data[0:12] + create_vlan_tag(int(vlan_src)) + data[12:]
+
+            # Same VLAN, therefore send; header remains the same
+            if vlan_src == vlan_dest:
+                send_to_link(dest_interface, length, data)
+
+            # Different VLAN (from access to trunk)
+            # Modify header and send
+            elif (vlan_dest == 'T' and 
+                  trunk_interfaces[dest_interface] == "Designated"):
+                send_to_link(dest_interface, len(tagged_frame), tagged_frame)
+
+        # Destination MAC is unknown
+        else:
+
+            # Calculate src VLAN and frame with tag only
+            vlan_src = vlan_table[interface]
+            tagged_frame = data[0:12] + create_vlan_tag(int(vlan_src)) + data[12:]
+
+            for i in interfaces:
+                vlan_dest = vlan_table[i]
+
+                # For same VLAN (access to access), just send
+                if i != interface and vlan_src == vlan_dest:
+                    send_to_link(i, length, data)
+
+                # For trunk port, update frame and send
+                elif (i != interface and vlan_dest == 'T' and 
+                      trunk_interfaces[i] == "Designated"):
+                    send_to_link(i, len(tagged_frame), tagged_frame)
+
+    # Broadcast
+    else:
+
+        # Calculate VLANs, frame with tag and destination interface
+        vlan_src = vlan_table[interface]
+        tagged_frame = data[0:12] + create_vlan_tag(int(vlan_src)) + data[12:]
+
+        for i in interfaces:
+            vlan_dest = vlan_table[i]
+
+            # For same VLAN (access to access), just send
+            if i != interface and vlan_src == vlan_dest:
+                send_to_link(i, length, data)
+
+            # For trunk port, update frame and send
+            elif (i != interface and vlan_dest == 'T' and 
+                  trunk_interfaces[i] == "Designated"):
+                send_to_link(i, len(tagged_frame), tagged_frame)
+
+# Switch receives a frame from trunk and forwards it
+def fwd_from_trunk(dest_mac, mac_table, vlan_table, vlan_id,
+                   interface, interfaces, data, length, trunk_interfaces):
+    # Unicast
+    if dest_mac != BROADCAST:
+
+        # Destination MAC is known
+        if dest_mac in mac_table:
+
+            # Calculate dest VLAN, original frame and destination interface
+            dest_interface = mac_table[dest_mac]
+            vlan_dest = vlan_table[dest_interface]
+            original_frame = data[0:12] + data[16:]
+            
+            # Same VLAN (trunk to trunk), therefore send; header remains the same
+            if (vlan_dest == 'T' and 
+                trunk_interfaces[dest_interface] == "Designated"):
+                send_to_link(dest_interface, length, data)
+
+            # Different VLAN (from trunk to access)
+            # Modify header and send
+            elif vlan_dest == str(vlan_id):
+                send_to_link(dest_interface, len(original_frame), original_frame)
+
+        # Destination MAC is unknown
+        else:
+
+            # Calculate original frame
+            original_frame = data[0:12] + data[16:]
+
+            for i in interfaces:
+                vlan_dest = vlan_table[i]
+
+                # For same VLAN (trunk to trunk), just send
+                if (i != interface and 
+                    vlan_dest == 'T' and 
+                    trunk_interfaces[i] == "Designated"):
+                    send_to_link(i, length, data)
+
+                # For access port, update frame and send
+                elif i != interface and vlan_dest == str(vlan_id):
+                    send_to_link(i, len(original_frame), original_frame)
+
+    # Broadcast
+    else:
+
+        # Calculate VLAN and original frame only
+        original_frame = data[0:12] + data[16:]
+
+        for i in interfaces:
+            vlan_dest = vlan_table[i]
+
+            # For same VLAN (trunk to trunk), just send
+            if (i != interface and 
+                vlan_dest == 'T' and 
+                trunk_interfaces[i] == "Designated"):
+                send_to_link(i, length, data)
+
+            # For access port, update frame and send
+            elif i != interface and vlan_dest == str(vlan_id):
+                send_to_link(i, len(original_frame), original_frame)
--- switch.py
+++ switch.py
@@ -4,11 +4,10 @@ import struct
 import wrapper
 import threading
 import time
-import helpers
 from wrapper import recv_from_any_link, send_to_link, get_switch_mac, get_interface_name
-from helpers import config_switch
+from helpers import stp_init, config_switch, handle_bdpu, send_frame, MULTICAST, BPDU
 
-BROADCAST = "ff:ff:ff:ff:ff:ff"
+ENABLE_PRINT = False
 
 def parse_ethernet_header(data):
     # Unpack the header fields from the byte array
@@ -28,14 +27,17 @@ def parse_ethernet_header(data):
 
     return dest_mac, src_mac, ether_type, vlan_id
 
-def create_vlan_tag(vlan_id):
-    # 0x8100 for the Ethertype for 802.1Q
-    # vlan_id & 0x0FFF ensures that only the last 12 bits are used
-    return struct.pack('!H', 0x8200) + struct.pack('!H', vlan_id & 0x0FFF)
-
-def send_bdpu_every_sec():
+def send_bdpu_every_sec(own_bridge_ID, root, vlan_table):
     while True:
-        # TODO Send BDPU every second if necessary
+        # Send BDPU every second if switch is root
+        if own_bridge_ID == root[0]:
+            for i in vlan_table:
+                if vlan_table[i] == 'T':
+                    message_age = 0x0001
+                    bpdu = BPDU(get_switch_mac(), root[0], root[1],
+                                own_bridge_ID, i, message_age)
+                    send_to_link(i, bpdu.len(), bpdu.pack())
+
         time.sleep(1)
 
 def main():
@@ -49,169 +51,56 @@ def main():
     print("# Starting switch with id {}".format(switch_id), flush=True)
     print("[INFO] Switch MAC", ':'.join(f'{b:02x}' for b in get_switch_mac()))
 
-    # Create and start a new thread that deals with sending BDPU
-    t = threading.Thread(target=send_bdpu_every_sec)
-    t.start()
-
     # Printing interface names
     for i in interfaces:
         print(get_interface_name(i))
 
     # Init necessary variables
-    # Mac table, vlan table
+    # Mac table, vlan table, STP variables
     mac_table = dict()
-    vlan_table, _ = config_switch(switch_id, interfaces)
+    vlan_table, own_bridge_ID = config_switch(switch_id, interfaces)
+    trunk_interfaces, root = stp_init(vlan_table, own_bridge_ID)
+
+    # ROOT[0] = root_bridge_ID
+    # ROOT[1] = root_path_cost
+    # ROOT[2] = root_port_ID
+
+    # Create and start a new thread that deals with sending BPDU
+    t = threading.Thread(target=send_bdpu_every_sec, args=(own_bridge_ID, root, vlan_table))
+    t.start()
 
     while True:
-        # Note that data is of type bytes([...]).
-        # b1 = bytes([72, 101, 108, 108, 111])  # "Hello"
-        # b2 = bytes([32, 87, 111, 114, 108, 100])  # " World"
-        # b3 = b1[0:2] + b[3:4].
+        # Note that data is of type bytes([...])
         interface, data, length = recv_from_any_link()
-
         dest_mac, src_mac, ethertype, vlan_id = parse_ethernet_header(data)
+
+        # Keep byte options too
+        dest_mac_bytes = dest_mac
+        src_mac_bytes = src_mac
 
         # Print the MAC src and MAC dst in human readable format
         dest_mac = ':'.join(f'{b:02x}' for b in dest_mac)
         src_mac = ':'.join(f'{b:02x}' for b in src_mac)
 
-        # Note. Adding a VLAN tag can be as easy as
-        # tagged_frame = data[0:12] + create_vlan_tag(10) + data[12:]
-
-        print(f'Destination MAC: {dest_mac}')
-        print(f'Source MAC: {src_mac}')
-        print(f'EtherType: {ethertype}')
-
-        print("Received frame of size {} on interface {}".format(length, interface), flush=True)
-
-        # TODO: Implement forwarding with learning
         #  Update source MAC in MAC_table
         mac_table[src_mac] = interface
 
-        # TODO: Implement VLAN support
+        if ENABLE_PRINT:
+            print(f'Destination MAC: {dest_mac}')
+            print(f'Source MAC: {src_mac}')
+            print(f'EtherType: {ethertype}')
 
-        # Forward from access or trunk port, depending on vlan_id
-        # Access
-        if vlan_id == -1:   
-       
-            # Unicast
-            if dest_mac != BROADCAST:
+            print("Received frame of size {} on interface {}".format(length, interface), flush=True)
 
-                # Destination MAC is known
-                if dest_mac in mac_table:
+        # Check for BPDU
+        if dest_mac == MULTICAST:
+            handle_bdpu(data, root, interface, trunk_interfaces,
+                        src_mac_bytes, own_bridge_ID)
 
-                    # Calculate VLANs, frame with tag and destination interface
-                    vlan_src = vlan_table[interface]
-                    dest_interface = mac_table[dest_mac]
-                    vlan_dest = vlan_table[dest_interface]
-                    tagged_frame = data[0:12] + create_vlan_tag(int(vlan_src)) + data[12:]
-
-                    # Same VLAN, therefore send; header remains the same
-                    if vlan_src == vlan_dest:
-                        send_to_link(dest_interface, length, data)
-
-                    # Different VLAN (from access to trunk)
-                    # Modify header and send
-                    elif vlan_dest == 'T':
-                        send_to_link(dest_interface, len(tagged_frame), tagged_frame)
-
-                # Destination MAC is unknown
-                else:
-
-                    # Calculate src VLAN and frame with tag only
-                    vlan_src = vlan_table[interface]
-                    tagged_frame = data[0:12] + create_vlan_tag(int(vlan_src)) + data[12:]
-
-                    for i in interfaces:
-                        vlan_dest = vlan_table[i]
-
-                        # For same VLAN (access to access), just send
-                        if i != interface and vlan_src == vlan_dest:
-                            send_to_link(i, length, data)
-
-                        # For trunk port, update frame and send
-                        elif i != interface and vlan_dest == 'T':
-                            send_to_link(i, len(tagged_frame), tagged_frame)
-
-            # Broadcast
-            else:
-
-                # Calculate VLANs, frame with tag and destination interface
-                vlan_src = vlan_table[interface]
-                tagged_frame = data[0:12] + create_vlan_tag(int(vlan_src)) + data[12:]
-
-                for i in interfaces:
-                    vlan_dest = vlan_table[i]
-
-                    # For same VLAN (access to access), just send
-                    if i != interface and vlan_src == vlan_dest:
-                        send_to_link(i, length, data)
-
-                    # For trunk port, update frame and send
-                    elif i != interface and vlan_dest == 'T':
-                        send_to_link(i, len(tagged_frame), tagged_frame)
-
-        # Trunk
+        # Normal frame
         else:
-
-            # Unicast
-            if dest_mac != BROADCAST:
-
-                # Destination MAC is known
-                if dest_mac in mac_table:
-
-                    # Calculate dest VLAN, original frame and destination interface
-                    dest_interface = mac_table[dest_mac]
-                    vlan_dest = vlan_table[dest_interface]
-                    original_frame = data[0:12] + data[16:]
-                    
-                    # Same VLAN (trunk to trunk), therefore send; header remains the same
-                    if vlan_dest == 'T':
-                        send_to_link(dest_interface, length, data)
-
-                    # Different VLAN (from trunk to access)
-                    # Modify header and send
-                    elif int(vlan_dest) == vlan_id:
-                        send_to_link(dest_interface, len(original_frame), original_frame)
-
-                # Destination MAC is unknown
-                else:
-
-                    # Calculate original frame
-                    original_frame = data[0:12] + data[16:]
-
-                    for i in interfaces:
-                        vlan_dest = vlan_table[i]
-
-                        # For same VLAN (trunk to trunk), just send
-                        if i != interface and vlan_dest == 'T':
-                            send_to_link(i, length, data)
-
-                        # For access port, update frame and send
-                        elif i != interface and int(vlan_dest) == vlan_id:
-                            send_to_link(i, len(original_frame), original_frame)
-
-            # Broadcast
-            else:
-
-                # Calculate VLAN and original frame only
-                original_frame = data[0:12] + data[16:]
-
-                for i in interfaces:
-                    vlan_dest = vlan_table[i]
-
-                    # For same VLAN (trunk to trunk), just send
-                    if i != interface and vlan_dest == 'T':
-                        send_to_link(i, length, data)
-
-                    # For access port, update frame and send
-                    elif i != interface and int(vlan_dest) == vlan_id:
-                        send_to_link(i, len(original_frame), original_frame)
-
-        # TODO: Implement STP support
-
-        # data is of type bytes.
-        # send_to_link(i, length, data)
+            send_frame(dest_mac, mac_table, vlan_table, vlan_id, 
+                       interface, interfaces, data, length, trunk_interfaces)
 
 if __name__ == "__main__":
     main()
